4afd9b3d8f79d0c5a7eaeb87878c6d44fe6dd7c5
ledat5934 <ledat5934@gmail.com>
2025-07-16 23:55:06 +0700

    Update guideline_create.py

--- guideline_create.py
+++ guideline_create.py
@@ -329,7 +329,7 @@ Please provide your response in JSON format. It is acceptable to provide an empt
     }}
 }}"""
 
-    return prompt
+    return old_prompt
 
 # --- END: PROMPT ĐÃ ĐƯỢC CẬP NHẬT ---
 
